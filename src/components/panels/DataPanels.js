7c4ab7a86e9079d87e3c2b36536472c3cbe00732
Jason Paul <jp555soul@gmail.com>
2025-08-18 10:09:02 -0700

    fix: https

--- src/components/panels/DataPanels.js
+++ src/components/panels/DataPanels.js
@@ -224,7 +224,7 @@ const DataPanels = ({
       
       <div className="col-span-2 md:col-span-2 lg:col-span-2 p-2 md:p-4 bg-gradient-to-r from-green-900/20 to-emerald-900/20 border-r border-green-500/10">
         <iframe 
-          src="https://jmpaul.com/" 
+          src="https://bluemvmt-holoocean-1.ngrok.dev/" 
           className="w-full h-full border-none rounded-lg"
           title="External Content"
           sandbox="allow-scripts allow-same-origin"
